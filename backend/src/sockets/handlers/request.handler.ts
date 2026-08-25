43c73de10f167cda6d2c1d8df82021ce3cacca2d
Tiago <tiagophilologos@gmail.com>
2026-08-25 17:26:19 -0300

    fix: add missing request decline handler in handle requests

--- backend/src/sockets/handlers/request.handler.ts
+++ backend/src/sockets/handlers/request.handler.ts
@@ -1,4 +1,8 @@
-import { acceptRequest, sendRequest } from "../../services/request.service.js";
+import {
+    acceptRequest,
+    rejectRequest,
+    sendRequest,
+} from "../../services/request.service.js";
 import type { AppServer, AppSocket } from "../index.js";
 import { socketAsyncHandler } from "../utils/socketAsyncHandler.js";
 
@@ -11,12 +15,27 @@ export function handleRequests(io: AppServer, socket: AppSocket) {
         }),
     );
 
-    socket.on("friend:accept", socketAsyncHandler(socket, "friend:error", async (payload) => {
-        const { from } = payload;
-        await acceptRequest({
-            io,
-            from,
-            userAccepting: socket.data.username,
-        });
-    }));
+    socket.on(
+        "friend:accept",
+        socketAsyncHandler(socket, "friend:error", async (payload) => {
+            const { from } = payload;
+            await acceptRequest({
+                io,
+                from,
+                userAccepting: socket.data.username,
+            });
+        }),
+    );
+
+    socket.on(
+        "friend:decline",
+        socketAsyncHandler(socket, "friend:error", async (payload) => {
+            const { from } = payload;
+            await rejectRequest({
+                io,
+                from,
+                userRejecting: socket.data.username,
+            });
+        }),
+    );
 }
